f97be668adf489658da7948c1be27161a6dcf81d
Naman deep singh <sardarparmar111@gmail.com>
2023-09-22 21:03:55 +0530

    added functionality to the category filter and changed the design of product card and lifted the bookmarklist state.

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -4,8 +4,9 @@ import Header from '../body/header/Header';
 import FilterProductsForm from "./filter_products_form/FilterProductsForm";
 import ProductList from '../body/product_list/ProductList';
 
-import { useState, useEffect } from "react";
-import WishList from './header/wish_list/WishList';
+import { useState/* , useEffect */ } from "react";
+import WishList from '../body/wish_list/WishList';
+import Cart from '../body/cart/Cart';
 
 /* import bg1 from '../body/img/formBG1.avif';
 import bg2 from '../body/img/formBG2.avif';
@@ -14,32 +15,56 @@ import bg4 from '../body/img/formBG4.avif';
 const images = [bg1, bg2, bg3, bg4]; */
 
 export default function Body() {
-    const [bookmarkList, setBookmarkList] = 
-        useState(
-            typeof localStorage.getItem('bookmarkedProducts') === 'undefined' || 
-                localStorage.getItem('bookmarkedProducts') === null ? 
-                []: JSON.parse(localStorage.getItem('bookmarkedProducts'))
-        );
+    const data = JSON.parse(localStorage.getItem('bookmarkedProducts')) || [];
+    const SHOPKART_PRODUCTS = PRODUCT_DATA.products;
+    const [products, setProducts] = useState(SHOPKART_PRODUCTS);
+    const [bookmarkList, setBookmarkList] = useState(data);
+    const categoryList = [];
+    Object.values(SHOPKART_PRODUCTS).forEach(product => {
+        const category = product.category.toLowerCase().trim();
+        if (!categoryList.includes(category))
+            categoryList.push(category);
+    });
+    const [category, setCategory] = useState('all');
     const bookmarkClickHandler = (event) => {
+        const bookmarkList1 = [...bookmarkList];
         const productId = event.target.closest('.card').getAttribute('product_id');
         if (productId && typeof productId === "string") {
-            console.log("Type of bookmarkList: ", typeof bookmarkList);
-            if (bookmarkList.includes(productId)) {
-                console.log(typeof bookmarkList);
-                bookmarkList.splice(bookmarkList.indexOf(productId), 1);
+            console.log("Type of bookmarkList: ", typeof bookmarkList1);
+            if (bookmarkList1.includes(productId)) {
+                console.log(typeof bookmarkList1);
+                bookmarkList1.splice(bookmarkList1.indexOf(productId), 1);
                 event.target.closest('.card').setAttribute('bookmarked', 'false');
             } else {
-                bookmarkList.push(productId);
+                bookmarkList1.push(productId);
                 event.target.closest('.card').setAttribute('bookmarked', 'true');
             }
-            console.log("bookmarkList: ", bookmarkList);
-            localStorage.setItem('bookmarkedProducts', JSON.stringify(bookmarkList));
-            setBookmarkList(bookmarkList)
+            setBookmarkList(bookmarkList1);
+            localStorage.setItem('bookmarkedProducts', JSON.stringify(bookmarkList1));
         } 
     }
-    useEffect(() => {
-        console.log("Bookmarklist: ", bookmarkList)
-    }, [bookmarkList])
+    const productCategoryClickHandler = (event) => {
+        const target_category = event.target?.dataset?.category.trim();
+        console.log(target_category);
+        if (target_category && typeof target_category === 'string' && target_category !== "") {
+            document.querySelector(".category_filter_list li[active='true']")?.setAttribute('active', 'false');
+            setCategory(target_category);
+            event.target.setAttribute('active', 'true');
+            let shopKartProducts = [...SHOPKART_PRODUCTS];
+            if (target_category === 'all') {
+                setProducts(shopKartProducts);
+            }
+            else {
+                shopKartProducts = shopKartProducts.filter(product => {
+                    return product.category.trim() === target_category;
+                });
+                setProducts(shopKartProducts);
+            }
+        }
+    }
+    /* useEffect(() => {
+        console.log("Bookmarklist1234: ", bookmarkList)
+    }, [bookmarkList]); */
     /* const [index, setIndex] = useState(1);
     useEffect(() => {
         console.log('PRODUCT_DATA: ', PRODUCT_DATA);
@@ -56,15 +81,20 @@ export default function Body() {
             <Header 
                 websiteName="shopkart"
             />
-            <WishList 
-                products={PRODUCT_DATA.products}
-                bookmarkList={bookmarkList} 
-            />
+            <div className="button_list" id="saved_items">
+                <WishList 
+                    products={products}
+                    bookmarkList={bookmarkList} 
+                />
+                <Cart />
+            </div>
             <FilterProductsForm />
             <ProductList 
-                products={PRODUCT_DATA.products} 
+                products={products} 
                 bookmarkList={bookmarkList} 
+                categoryList={categoryList}
                 bookmarkClickHandler={bookmarkClickHandler}
+                categoryClickHandler={productCategoryClickHandler}
             />
         </div>
     );
---
+++ src/components/body/wish_list/WishList.jsx
@@ -0,0 +1,28 @@
+export default function WishList({ products, bookmarkList }) {
+    return (
+        <div id="wish_list_container">
+            <button className="btn btn-warning show_wish_list_item" data-target="widh_list">
+                <span className="btn-txt">Wish List</span>
+                <div className="btn-svg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="2">
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z" fill="red">
+                            </path>
+                        </g>
+                    </svg>
+                </div>
+            </button>
+            <ul id="widh_list" className="dnone">
+                {
+                  products && products.length > 0 && bookmarkList && bookmarkList.length > 0 && products.filter((product) => {
+                        return bookmarkList.includes(product.id.toString());
+                    }).map((bookmarkedProduct) => {
+                        return <li key={bookmarkedProduct.id}>{bookmarkedProduct.id}</li>
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
